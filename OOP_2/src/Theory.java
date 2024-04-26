cde99dcee2d96345369ca343506311806682623e
Moriarty <tphong610198@gmail.com>
2024-04-26 19:07:34 +0700

    commited by Moriarty

---
+++ OOP_2/src/Theory.java
@@ -0,0 +1,12 @@
+/*
+click https://www.programiz.com/java-programming/inheritance to read about inheritance
+click https://www.programiz.com/java-programming/method-overriding to read about Method Overriding
+click https://www.programiz.com/java-programming/super-keyword to read about "super" keyword
+click https://www.programiz.com/java-programming/abstract-classes-methods to read about abstract class
+click https://www.programiz.com/java-programming/interfaces to read about interface
+click https://www.programiz.com/java-programming/polymorphism to read about polymorphism
+click https://www.programiz.com/java-programming/encapsulation to read about encapsulation
+ */
+
+
+
